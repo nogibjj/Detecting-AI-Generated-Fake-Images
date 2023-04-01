29eee1c861e0f6263f2a93400584fe7cf9c67120
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-03-31 21:37:35 -0400

    edit py

--- dalle2.py
+++ dalle2.py
@@ -16,18 +16,18 @@ def image_to_image():
     print(image_url)
     # return image_url
 
-def edit_image():
-    response = openai.Image.create_edit(
-    image=open("/Users/scottlai/Desktop/coding_project/facerecognition/pic_try/00001.png", "rb"),
-    mask=open("/Users/scottlai/Desktop/coding_project/facerecognition/pic_try/00000.png", "rb"),
-    prompt="change the face to a different emotion",
-    n=3,
-    size="1024x1024"
-    )
-    image_url = response['data'][0]['url']
-    print(image_url)
+# def edit_image():
+#     response = openai.Image.create_edit(
+#     image=open("/Users/scottlai/Desktop/coding_project/facerecognition/pic_try/00001.png", "rb"),
+#     mask=open("/Users/scottlai/Desktop/coding_project/facerecognition/pic_try/00000.png", "rb"),
+#     prompt="change the face to a different emotion",
+#     n=3,
+#     size="1024x1024"
+#     )
+#     image_url = response['data'][0]['url']
+#     print(image_url)
     
     
 if __name__ == "__main__":
-    # image_to_image()
-    edit_image()
+    image_to_image()
+    # edit_image()
