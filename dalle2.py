f4c8398eec8f96ce1613710115b2964d95580767
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-03-31 21:13:51 -0400

    add py

---
+++ dalle2.py
@@ -0,0 +1,31 @@
+import os
+from PIL import Image
+import openai
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+def image_to_image():
+    response = openai.Image.create_variation(
+    image=open("/Users/scottlai/Desktop/coding_project/facerecognition/pic_try/00000.png", "rb"),
+    n=1,
+    size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    print(image_url)
+    # return image_url
+
+def edit_image():
+    response = openai.Image.create_edit(
+    image=open("/Users/scottlai/Desktop/coding_project/facerecognition/pic_try/00001.png", "rb"),
+    mask=open("/Users/scottlai/Desktop/coding_project/facerecognition/pic_try/00000.png", "rb"),
+    prompt="change the face to a different emotion",
+    n=3,
+    size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    print(image_url)
+    
+    
+if __name__ == "__main__":
+    # image_to_image()
+    edit_image()
