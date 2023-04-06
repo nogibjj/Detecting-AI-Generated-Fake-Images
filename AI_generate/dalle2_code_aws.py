df1ecd9e9f53da7333bf08a39551982d65fed37f
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-04-06 04:04:33 +0000

    add file

--- AI_generate/dalle2_code_aws.py
+++ AI_generate/dalle2_code_aws.py
@@ -1,15 +1,64 @@
 import boto3
 import os
-from dotenv import load_dotenv
+import requests
+import openai
+from PIL import Image
+import random
+import json
+import pandas as pd
+import io
 
-load_dotenv()
+
 session = boto3.Session(
-    aws_access_key_id = os.environ('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key = os.environ('AWS_SECRET_ACCESS_KEY'),
-    region_name='us-west-2'
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    region_name='us-east-1'
 )
-s3 = session.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
+
+s3 = session.client('s3')
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
+def generate_image(input_image_path, image_name):
+
+    image_content = None
+    with open(input_image_path, "rb") as f:
+        image_content = f.read()
+
+    # Generate the AI images
+    response = openai.Image.create_variation(
+        image=image_content,
+        n=1,
+        size="1024x1024"
+    )
+
+    image_url = [data['url'] for data in response['data']][0]
+    print(f"Image URL: {image_url}")
+
+    image_data = requests.get(image_url).content
+    with open(f"fake_{input_image_path}", "wb") as f:
+        f.write(image_data)
+
+    s3.upload_file(f"fake_{input_image_path}",
+                   "dalle2images", f"fake/{image_name}")
+
+
+def main():
+
+    images_df = pd.read_csv('df_final.csv')
+    image_names = list(images_df["Name"])
+
+    for image_name in image_names:
+        s3.download_file('dalle2images', f'real/{image_name}', image_name)
+        generate_image(image_name, image_name)
+        if (os.path.exists(image_name)):
+            os.remove(image_name)
+        if (os.path.exists(f"fake_{image_name}")):
+            os.remove(f"fake_{image_name}")
+        images_df = images_df.loc[images_df["Name"] != image_name]
+        images_df.to_csv("df_final.csv")
+
+
+if __name__ == "__main__":
+    main()
--- AI_generate/dalle2_code_google.py
+++ AI_generate/dalle2_code_google.py
@@ -16,8 +16,8 @@ load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Set up the Google Drive API client
-credentials = service_account.Credentials.from_service_account_file('dalle2aigenerator-f4cbc4432631.json', scopes=['https://www.googleapis.com/auth/drive'])
-drive_service = build('drive', 'v3', credentials=credentials)
+# credentials = service_account.Credentials.from_service_account_file('dalle2aigenerator-f4cbc4432631.json', scopes=['https://www.googleapis.com/auth/drive'])
+# drive_service = build('drive', 'v3', credentials=credentials)
 
 new_df = pd.read_csv('face.csv')
 
@@ -38,18 +38,23 @@ def save_original_photo_to_drive(input_image_path, folder_id, photo_title):
         'parents': [folder_id]
     }
     media = MediaFileUpload(temp_image_path, mimetype='image/png')
-    file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    print(f"Uploaded original image to Google Drive with File ID: {file.get('id')}")
+    file = drive_service.files().create(
+        body=file_metadata, media_body=media, fields='id').execute()
+    print(
+        f"Uploaded original image to Google Drive with File ID: {file.get('id')}")
 
     # Delete the temporary file
     os.remove(temp_image_path)
 
     return file.get('id')
 
+
 def generate_image(input_image_path, n, photo_title):
     # Save the original photo to Google Drive
-    human_folder_id = "1ZjumcbLeUY7KK6nDf2y2CvzJo6CtEprh"  # Replace with the ID of the "human" folder
-    original_photo_file_id = save_original_photo_to_drive(input_image_path, human_folder_id, photo_title)
+    # Replace with the ID of the "human" folder
+    human_folder_id = "1ZjumcbLeUY7KK6nDf2y2CvzJo6CtEprh"
+    original_photo_file_id = save_original_photo_to_drive(
+        input_image_path, human_folder_id, photo_title)
 
     # Generate the AI images
     response = openai.Image.create_variation(
@@ -73,7 +78,8 @@ def generate_image(input_image_path, n, photo_title):
             f.write(image_data)
 
         # Upload the image to Google Drive
-        folder_id = "15hNp5EiJbOTtRATX_7ugbdKVb-1guQdd"  # Replace with the ID of the folder where you want to save generated images
+        # Replace with the ID of the folder where you want to save generated images
+        folder_id = "15hNp5EiJbOTtRATX_7ugbdKVb-1guQdd"
         file_metadata = {
             'name': f"{photo_title}_ai({i+1}).png",
             'mimeType': 'image/png',
@@ -81,13 +87,17 @@ def generate_image(input_image_path, n, photo_title):
         }
 
         media = MediaFileUpload(temp_image_path, mimetype='image/png')
-        file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        file = drive_service.files().create(
+            body=file_metadata, media_body=media, fields='id').execute()
         print(f"Uploaded image to Google Drive with File ID: {file.get('id')}")
 
         # Update the DataFrame with the original and generated image URLs
-        new_df.loc[new_df['file_url'] == input_image_path, 'original_photo_file_id'] = original_photo_file_id
-        new_df.loc[new_df['file_url'] == input_image_path, 'generated_image_url'] = image_url
-        new_df.loc[new_df['file_url'] == input_image_path, 'generated_image_file_id'] = file.get('id')
+        new_df.loc[new_df['file_url'] == input_image_path,
+                   'original_photo_file_id'] = original_photo_file_id
+        new_df.loc[new_df['file_url'] == input_image_path,
+                   'generated_image_url'] = image_url
+        new_df.loc[new_df['file_url'] == input_image_path,
+                   'generated_image_file_id'] = file.get('id')
 
         # Delete the temporary file
         os.remove(temp_image_path)
@@ -109,9 +119,9 @@ def main(output_folder, num_images):
         print(f"Processing image: {input_image_url}")
         generate_image(input_image_url, 1, photo_title)
 
-
     # Save the updated DataFrame to a new CSV file
     new_df.to_csv("updated_ffhq-dataset-v2.csv", index=False)
+
 
 if __name__ == "__main__":
     output_folder = "aigenerate"
