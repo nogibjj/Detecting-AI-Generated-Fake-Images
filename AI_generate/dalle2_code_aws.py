c153e95bf69b5f7b524a749a8b8d7972053fb9d6
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-04-06 02:09:37 +0000

    label

--- AI_generate/dalle2_code_aws.py
+++ AI_generate/dalle2_code_aws.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 session = boto3.Session(
-    aws_access_key_id = os.getenv('AWS_Access_Key_ID'),
-    aws_secret_access_key = os.getenv('AWS_Secret_Access_Key'),
+    aws_access_key_id = os.environ('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key = os.environ('AWS_SECRET_ACCESS_KEY'),
     region_name='us-west-2'
 )
 s3 = session.resource('s3')
 for bucket in s3.buckets.all():
     print(bucket.name)
+
+
