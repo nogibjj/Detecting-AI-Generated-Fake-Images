d6bcb982f660c55d4b1571a87d2303f05836851d
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-04-17 21:06:43 -0400

    add visualization

--- model_train/MobileNetV2_model.py
+++ model_train/MobileNetV2_model.py
@@ -1,14 +1,14 @@
 import boto3
-import tensorflow as tf
+import tensorflow.compat.v2 as tf
 from PIL import Image
 import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.regularizers import l2
+from umap import UMAP
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
-
-
-
+tf.keras.backend.set_image_data_format('channels_first')
 
 s3 = boto3.resource('s3')
 bucket_name = 'dalle2images'
@@ -25,16 +25,16 @@ for obj in s3.Bucket(bucket_name).objects.all():
     elif obj.key.endswith('.png') and obj.key.startswith('fake'):
         fake_image_paths.append(obj.key)
         fake_labels.append(1)
-
-
-# randomly select 1000 images (500 real, 500 fake) from your bucket
-n_images = 300
+        
+# randomly select 100 images (50 real, 50 fake) from your bucket
+n_images = 100
 n_real = n_fake = n_images // 2
 real_samples = np.random.choice(real_image_paths, n_real, replace=False)
 fake_samples = np.random.choice(fake_image_paths, n_fake, replace=False)
 sample_paths = np.concatenate([real_samples, fake_samples])
 
 images = []
+orig_images = []  # Added: Store original images before preprocessing
 labels = []
 image_size = (224, 224)
 
@@ -42,15 +42,23 @@ for i, path in enumerate(sample_paths):
     img = s3.Object(bucket_name, path)
     img = Image.open(img.get()['Body'])
     img = img.resize(image_size, Image.ANTIALIAS)
-    img = np.array(img)
-    img = tf.cast(img, dtype=tf.float32)
+
+    orig_img = np.array(img)  # Added: Store original image before preprocessing
+    orig_images.append(orig_img)  # Added: Append original image to the list
+
+    img = np.array(img).transpose((2, 0, 1))  # Change image format to channels_first
+    img = img[..., ::-1]  # Convert from RGB to BGR format
+    img = preprocess_input(img)
+
     images.append(img)
     labels.append(0 if path.startswith('real') else 1)
 
 images = np.stack(images)
+orig_images = np.stack(orig_images)  # Added: Stack original images
 labels = np.array(labels)
 
 train_images, test_images, train_labels, test_labels = train_test_split(images, labels, test_size=0.2, random_state=42)
+train_orig_images, test_orig_images, _, _ = train_test_split(orig_images, labels, test_size=0.2, random_state=42)  # Added: Split original images
 
 train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
 test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
@@ -61,9 +69,10 @@ batch_size = 32
 train_dataset = train_dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
 test_dataset = test_dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
 
-
 # Load the pre-trained MobileNetV2 model without the top layers
-base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(3, 224, 224))
+
+
 
 # Freeze the base model's layers
 for layer in base_model.layers:
@@ -104,15 +113,37 @@ history = model.fit(train_dataset, epochs=epochs, validation_data=test_dataset)
 loss, accuracy = model.evaluate(test_dataset)
 print("Test accuracy: {:.2f}%".format(accuracy * 100))
 
+# # Extract the features from the trained model for t-SNE visualization
+# features = model.predict(test_images)
 
-# Extract the features from the trained model for t-SNE visualization
-features = model.predict(test_images)
+# umap = UMAP(n_components=2, random_state=42)
+# umap_features = umap.fit_transform(features)
 
-# Use t-SNE to reduce the dimensionality of the feature vectors to 2D
-from sklearn.manifold import TSNE
-tsne = TSNE(n_components=2, verbose=1, perplexity=5)
-tsne_features = tsne.fit_transform(features)
-# Plot the t-SNE features with different colors for real and fake images
-import matplotlib.pyplot as plt
-plt.scatter(tsne_features[:, 0], tsne_features[:, 1], c=test_labels, cmap='cool')
-plt.show()
+# import matplotlib.pyplot as plt
+# import matplotlib.offsetbox as offsetbox
+
+
+# fig, ax = plt.subplots(figsize=(10, 10))
+
+# # Scaling factor to control the size of the displayed images
+# scale_factor = 0.1
+# border_thickness = 1
+
+# # Modify the visualization loop to use test_orig_images instead of test_images
+# for i, (x, y) in enumerate(umap_features):
+#     img = test_orig_images[i]  # Load the original image
+    
+#     # Resize the image and add a colored border
+#     img_resized = Image.fromarray(img).resize((int(img.shape[0] * scale_factor), int(img.shape[1] * scale_factor)), Image.ANTIALIAS)
+#     border_color = 'red' if test_labels[i] == 1 else 'green'
+#     bordered_image = Image.new('RGB', (img_resized.width + 2 * border_thickness, img_resized.height + 2 * border_thickness), border_color)
+#     bordered_image.paste(img_resized, (border_thickness, border_thickness))
+    
+#     # Add the image to the plot
+#     imagebox = offsetbox.AnnotationBbox(offsetbox.OffsetImage(bordered_image), (x, y), frameon=False)
+#     ax.add_artist(imagebox)
+
+# # Set the plot limits and display the plot
+# ax.set_xlim(umap_features[:, 0].min() - 5, umap_features[:, 0].max() + 5)
+# ax.set_ylim(umap_features[:, 1].min() - 5, umap_features[:, 1].max() + 5)
+# plt.show()
--- web_app/app.py
+++ web_app/app.py
@@ -6,13 +6,28 @@ from PIL import Image
 import streamlit as st
 import openai
 from io import BytesIO
+from huggingface_hub import from_pretrained_keras
+import numpy as np
+import tensorflow as tf
 
-# Load the OpenAI API key
-# load_dotenv()
 
+# Function to load models
+@st.cache(allow_output_mutation=True)
+def load_models():
+    models = {}
+    
+    model = from_pretrained_keras('scottlai/model_v2_fake_image_detection')
+    model.compile(optimizer='adam', loss='binary_crossentropy')
+    models["scottlai/model_v2_fake_image_detection"] = model
+    
+    model = from_pretrained_keras("poojakabber1997/ResNetDallE2Fakes")
+    model.compile(optimizer='adam', loss='binary_crossentropy')
+    models["poojakabber1997/ResNetDallE2Fakes"] = model
+
+    return models
 
 # Streamlit app
-st.title("AI Human Face Generator")
+
 
 content_loaded = False
 if "openai_api_key" not in st.session_state:
@@ -31,10 +46,15 @@ if content_loaded:
     openai.api_key = st.session_state.openai_api_key
 
     # Function to generate AI image
-    def generate_image(input_image_file):
+    def generate_image(input_image):
+        # Convert the input image to JPEG format
+        input_image = input_image.convert("RGB")
+        input_image_file = BytesIO()
+        input_image.save(input_image_file, format="PNG")
+
         # Generate the AI image
         response = openai.Image.create_variation(
-            image=input_image_file,
+            image=input_image_file.getvalue(),
             n=1,
             size="1024x1024"
         )
@@ -45,28 +65,109 @@ if content_loaded:
 
         return Image.open(BytesIO(image_data))
 
+    # Update the get_prediction function to accept the model's input shape
+    def get_prediction(image, model):
+        input_shape = model.input_shape
+        image = Image.fromarray(image.astype('uint8'), 'RGB').resize(input_shape[1:3])
+        image = np.array(image).astype(np.float32)
+        image = image / 255
+        image = np.expand_dims(image, axis=0)
 
+        prediction = model.predict(image)
+        real_prob = prediction[0][0]
+        fake_prob = 1 - real_prob
+
+        if real_prob > fake_prob:
+            return "Real Human Face", real_prob
+        else:
+            return "AI Generated Face", fake_prob
+
+
+
+    # Load both models at the beginning
+    models = load_models()
+
+    # Add a new sidebar option for model selection
     model_choice = st.sidebar.radio(
         "Select a model:",
-        ("AI Human Face Generator", "Real vs AI Human Face Detection")
+        (
+            "AI Human Face Generator",
+            "Real vs AI Human Face Detection - MobileNetV2 Model",
+            "Real vs AI Human Face Detection - RestNet Model"
+        ),
     )
 
+
     if model_choice == "AI Human Face Generator":
+        st.title("AI Human Face Generator") 
         uploaded_file = st.file_uploader("Choose an image file (png or jpg)", type=["png", "jpg"])
 
         if uploaded_file is not None:
-            input_image = Image.open(uploaded_file)
-
             st.header("Input Image")
             col1, col2 = st.columns(2)
 
+            # Resize the input image to have equal width and height
+            input_image = Image.open(uploaded_file)
+            width, height = input_image.size
+            new_size = min(width, height)
+            input_image = input_image.resize((new_size, new_size))
             col1.image(input_image, use_column_width=True)
 
             if st.button("Generate AI Image"):
                 uploaded_file.seek(0)  # Reset the file pointer to the beginning
-                ai_image = generate_image(uploaded_file)
+                ai_image = generate_image(input_image)
                 st.header("AI Generated Image")
                 col2.image(ai_image, use_column_width=True)
 
-    elif model_choice == "Real vs AI Human Face Detection":
-        st.write("This model is still in development.")
+        # Update the elif clause for Real vs AI Human Face Detection
+    elif model_choice.startswith("Real vs AI Human Face Detection"):
+        # Determine the selected model and its input shape
+        if model_choice.endswith("scottlai"):
+            model_key = "scottlai/model_v2_fake_image_detection"
+            input_shape = (1, 3, 224, 224)
+        else:
+            model_key = "poojakabber1997/ResNetDallE2Fakes"
+            input_shape = (1, 3, 180, 180)
+
+        # Use the selected model for predictions
+        model_fake_detection = models[model_key]
+       
+
+        st.title("Real vs AI Human Face Detection")
+        st.write("This is a demo of a real vs AI human face detection app using a MobileNetV2 model trained on the Real vs AI face detection dataset.")
+        st.write("Upload an image to see if it's a real human face or an AI generated one.")
+        st.write("")
+        uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg"])
+        
+        if uploaded_file is not None:
+            uploaded_array = np.array(Image.open(uploaded_file).convert("RGB"))
+            input_image = Image.open(uploaded_file)
+            width, height = input_image.size
+            new_size = min(width, height)
+            input_image = input_image.resize((new_size, new_size))
+            col1, col2 = st.columns([2, 1])
+            col1.image(input_image, use_column_width=True)
+            col2.write("Prediction Results:")
+            prediction, real_prob = get_prediction(uploaded_array, model_fake_detection)
+
+            # Get the prediction probabilities
+            real_prob = real_prob
+            fake_prob = 1-real_prob
+            
+            real_accuracy = "This is a Real Human Face"
+            real_probability = real_prob 
+        
+            fake_accuracy = "This is a AI Generated Face"
+            fake_probability = fake_prob
+            
+            if real_probability > 0.77:
+                col2.markdown(f"### {real_accuracy}")
+                col2.write(f"Real Probability: {real_probability}")
+            else:
+                col2.markdown(f"### {fake_accuracy}")
+                col2.write(f"Real Probability: {fake_probability}")
+
+
+
+
+
